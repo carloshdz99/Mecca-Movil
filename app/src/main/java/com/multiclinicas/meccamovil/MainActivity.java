98f6f2e0a0d304377a8f51670f2762b7bc702651
carloshdz99 <carloshdzfunes@gmail.com>
2021-04-12 16:59:34 -0600

    pantalla login y registro

--- app/src/main/java/com/multiclinicas/meccamovil/MainActivity.java
+++ app/src/main/java/com/multiclinicas/meccamovil/MainActivity.java
@@ -2,13 +2,40 @@ package com.multiclinicas.meccamovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    //botones
+    private Button btnLoginEmail, btnLoginRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_MeccaMovil);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        //inicializando variables del layout
+        Init();
+
+        //seteando metodos de botones
+        btnLoginRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    //metodo para iniciar variables
+    private void Init(){
+        //botones
+        btnLoginEmail = findViewById(R.id.btnLoginEmail);
+        btnLoginRegister = findViewById(R.id.btnLoginRegister);
     }
 }
--- app/src/main/java/com/multiclinicas/meccamovil/RegisterActivity.java
+++ app/src/main/java/com/multiclinicas/meccamovil/RegisterActivity.java
@@ -2,13 +2,39 @@ package com.multiclinicas.meccamovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    //botones
+    private Button btnRegisterEmail, btnRegisterLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
+
+        //inicializando variables del layout
+        Init();
+
+        //seteando botones de eventos
+        btnRegisterLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    //metodo para iniciar variables
+    private void Init(){
+        //botones
+        btnRegisterEmail = findViewById(R.id.btnRegisterEmail);
+        btnRegisterLogin = findViewById(R.id.btnRegisterLogin);
     }
 }
