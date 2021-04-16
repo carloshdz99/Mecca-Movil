c594ec683e8d8593b3f0d6cb74bb9b035220efaf
chris7sx <christianjflores7@gmail.com>
2021-04-16 15:38:38 -0600

    login google

--- app/src/main/java/com/multiclinicas/meccamovil/RegisterActivity.java
+++ app/src/main/java/com/multiclinicas/meccamovil/RegisterActivity.java
@@ -1,16 +1,35 @@
 package com.multiclinicas.meccamovil;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
     //botones
-    private Button btnRegisterEmail, btnRegisterLogin;
+    private Button btnRegisterEmail, btnRegisterLogin, googleBtn;
+    private static final int gSignIn = 100;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +48,37 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == gSignIn) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if (account != null) {
+                    AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                    FirebaseAuth.getInstance().signInWithCredential(credential)
+                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(), "Se registró con Google", Toast.LENGTH_LONG).show();
+
+                                    }
+                                    Toast.makeText(getApplicationContext(), "Ocurrió un error", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                }
+
+            } catch (ApiException e) {
+
+            }
+        }
     }
 
     //metodo para iniciar variables
@@ -36,5 +86,19 @@ public class RegisterActivity extends AppCompatActivity {
         //botones
         btnRegisterEmail = findViewById(R.id.btnRegisterEmail);
         btnRegisterLogin = findViewById(R.id.btnRegisterLogin);
+        googleBtn = findViewById(R.id.googleButton);
+    }
+
+    public void googleSesion(View view) {
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        Intent signinIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signinIntent, gSignIn);
+        mGoogleSignInClient.signOut();
     }
 }
